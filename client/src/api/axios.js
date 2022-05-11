2f77416f5a3586e3be28cc49bd6f955c10542562
chankyu kim <chank07@naver.com>
2022-05-11 15:08:19 +0900

    Feat) axios base API 생성

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,5 +1,10 @@
 import axios from 'axios';
+import { API_URL } from '../config/config';
 
 export default axios.create({
-  baseURL: 'http://localhost:4000',
+  baseURL: API_URL,
+  params: {
+    api_key: process.env.REACT_APP_API_KEY,
+    languag: 'en-US',
+  },
 });
