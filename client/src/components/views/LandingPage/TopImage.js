85a0a219d6f5d158b2c2ea8c7d546a054ec48d82
chankyu kim <chank07@naver.com>
2022-05-11 15:06:56 +0900

    Design) 최상단 이미지, 타이틀, 설명 추가

---
+++ client/src/components/views/LandingPage/TopImage.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Section = styled.section`
+  max-width: 100%;
+  height: 50rem;
+  /* background-image: url('https://picsum.photos/seed/picsum/1000/1000'); */
+  background-image: ${props => `url(${props.image})`};
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  padding: 4.8rem 3.6rem;
+
+  display: flex;
+  align-items: flex-end;
+
+  div {
+    color: #fff;
+    max-width: 50%;
+    display: flex;
+    flex-direction: column;
+  }
+
+  h2 {
+    font-size: 3.6rem;
+    font-weight: 700;
+    margin-bottom: 2.2rem;
+  }
+
+  p {
+    font-size: 1.4rem;
+    line-height: 1.6;
+  }
+`;
+
+const TopImage = ({ title, overview, image }) => {
+  return (
+    <Section image={image}>
+      <div>
+        <h2>{title}</h2>
+        <p>{overview}</p>
+      </div>
+    </Section>
+  );
+};
+
+export default TopImage;
