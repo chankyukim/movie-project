e9c44e8f5c1e9a952892673db61483586804a05d
chankyu kim <chank07@naver.com>
2022-05-12 09:24:51 +0900

    Feat) 랜딩페이지 구현

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,7 +1,68 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import axios from '../../../api/axios';
+import { API_URL, IMAGE_BASE_URL } from '../../../config/config';
+import Header from './Header';
+import MainSection from './MainSection';
+import TopImage from './TopImage';
 
 const LandingPage = () => {
-  return <div>Landing Page</div>;
+  const [movies, setMovies] = useState([]);
+  const [topImage, setTopImage] = useState(null);
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const abortController = new AbortController();
+
+  const fetchMovies = async endPoint => {
+    try {
+      const response = await axios.get(endPoint, {
+        signal: abortController.signal,
+      });
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    let isMounted = true;
+    const END_POINT = `${API_URL}movie/popular?page=1`;
+
+    fetchMovies(END_POINT) //
+      .then(response => {
+        isMounted && setMovies([...response.results]);
+        isMounted && setTopImage(response.results[0]);
+        setCurrentPage(response.page);
+      });
+
+    return () => {
+      isMounted = false;
+      abortController.abort();
+    };
+  }, []);
+
+  const loadMoreItem = () => {
+    const END_POINT = `${API_URL}movie/popular?page=${currentPage + 1}`;
+
+    fetchMovies(END_POINT) //
+      .then(response => {
+        setMovies([...movies, ...response.results]);
+        setCurrentPage(currentPage + 1);
+      });
+  };
+
+  return (
+    <div>
+      <Header />
+      {topImage && (
+        <TopImage
+          title={topImage.original_title}
+          overview={topImage.overview}
+          image={`${IMAGE_BASE_URL}w1280/${topImage.backdrop_path}`}
+        />
+      )}
+      <MainSection loadMoreItem={loadMoreItem} movies={movies} />
+    </div>
+  );
 };
 
 export default LandingPage;
