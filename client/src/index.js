a200bf5a54b94aa4a037f4c54fece88aa36a97c5
chankyu kim <chank07@naver.com>
2022-05-17 00:34:08 +0900

    Feat) 로그인 Authentication Context API 추가

--- client/src/index.js
+++ client/src/index.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
 import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import reduxThunk from 'redux-thunk';
 import rootReducer from './modules';
 import { GlobalStyle } from './styles';
+import { AuthProvider } from './context/AuthProvider';
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(reduxThunk)));
+// const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(reduxThunk)));
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
+    {/* <Provider store={store}> */}
+    <AuthProvider>
       <GlobalStyle />
       <App />
-    </Provider>
+    </AuthProvider>
+    {/* </Provider> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
