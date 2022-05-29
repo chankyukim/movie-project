0757303ef3f0debf5368a70b9ee3649fd4c85ad7
chankyu kim <chank07@naver.com>
2022-05-30 05:49:28 +0900

    Feat) 커스텀 axios 작성

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,10 +1,20 @@
 import axios from 'axios';
-import { API_URL } from '../config/config';
+import { API_URL, BASE_URL } from '../config/config';
 
-export default axios.create({
+export const axiosApi = axios.create({
   baseURL: API_URL,
   params: {
     api_key: process.env.REACT_APP_API_KEY,
     languag: 'en-US',
   },
+});
+
+export const axiosServer = axios.create({
+  baseURL: BASE_URL,
+});
+
+export const axiosPrivate = axios.create({
+  baseURL: BASE_URL,
+  headers: { 'Content-Type': 'application/json' },
+  withCredentials: true,
 });
